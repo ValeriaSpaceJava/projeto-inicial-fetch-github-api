caafb4c6b1be82a99724b331b8550e0c9548292c
Valéria Barreto <95004439+ValeriaSpaceJava@users.noreply.github.com>
2024-10-16 09:50:26 -0300

    enderecos-relativos

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,4 +1,4 @@
-import {baseUrl, maxItems} from '/src/scripts/variables.js'
+import {baseUrl, maxItems} from '../variables.js'
 
 async function getEvents(userName){
     const response = await fetch(`${baseUrl}/${userName}/events?per_page=${maxItems}`)
